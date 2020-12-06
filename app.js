2cddff8261a9e48b710ba4a4915d431f0451fdaf
Alfredo Aguirre <thefurok@gmail.com>
2020-12-06 01:35:53 -0500

    aplicacion de nodejs usando los funamentos

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 //const multiplicar = require('./multiplicar/multiplicar')
-const {crearArchivo, imprimirTabla} = require('./multiplicar/multiplicar')
-const yarg = require('./config/yargs').argv;
-
+const { crearArchivo, imprimirTabla } = require("./multiplicar/multiplicar");
+const yarg = require("./config/yargs").argv;
 
 let comando = yarg._[0];
 
-switch(comando){
-    case 'crear':
-        crearArchivo(yarg.base, yarg.limite)
-        .then(mensaje => {
-            console.log(mensaje);
-        }).catch(e => {
-            console.log(e);
-        });
-        break;
-    case 'imprimir':
-        imprimirTabla(yarg.base, yarg.limite).catch(e => {
-                console.log(e);
-            });
-            break;
+switch (comando) {
+  case "crear":
+    crearArchivo(yarg.base, yarg.limite)
+      .then((mensaje) => {
+        console.log(mensaje);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    break;
+  case "imprimir":
+    imprimirTabla(yarg.base, yarg.limite).catch((e) => {
+      console.log(e);
+    });
+    break;
 }
-
-
--- config/yargs.js
+++ config/yargs.js
@@ -1,21 +1,25 @@
 let opciones = {
-    base: {
-        demand: true,
-        alias: 'b',
-        desc: 'La base de la tabla de multiplicar'
-    },
-    limite: {
-        alias: 'l',
-        default: 10,
-        desc: 'Limite de la tabla de multiplicar'
-    }
+  base: {
+    demand: true,
+    alias: "b",
+    desc: "La base de la tabla de multiplicar",
+  },
+  limite: {
+    alias: "l",
+    default: 10,
+    desc: "Limite de la tabla de multiplicar",
+  },
 };
 
-const argv = require('yargs')
-    .command ('imprimir', 'Imprime en la consola la tabla de multiplicar', opciones)
-    .command ('crear', 'Crea un archivo de la tabla de multiplicar', opciones)
-    .argv;
+const argv = require("yargs")
+  .command(
+    "imprimir",
+    "Imprime en la consola la tabla de multiplicar",
+    opciones
+  )
+  .command("crear", "Crea un archivo de la tabla de multiplicar", opciones)
+  .argv;
 
 module.exports = {
-    argv
-}
+  argv,
+};
